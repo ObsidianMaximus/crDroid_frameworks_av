6e095132a9d85654a77797cd0f89a58f230395a0
Obsidian Maximus <obsidianmaximus@pm.me>
2024-08-15 08:28:14 +0530

    libcameraservice: Add support to set vendor tag with client package name
    * OEMs like OnePlus and Nothing detect camera package name to unlock features like 48mp.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored by: pjgowtham <pjgowtham@gmail.com>

--- services/camera/libcameraservice/device3/Camera3Device.cpp
+++ services/camera/libcameraservice/device3/Camera3Device.cpp
@@ -2460,7 +2460,7 @@ status_t Camera3Device::configureStreamsLocked(int operatingMode,
         return BAD_VALUE;
     }
 
-#ifdef CAMERA_PACKAGE_NAME
+#ifdef TAG_NAME
     sp<VendorTagDescriptor> vTags;
     sp<VendorTagDescriptorCache> vCache = VendorTagDescriptorCache::getGlobalVendorTagCache();
     if (vCache.get()) {
@@ -2469,8 +2469,8 @@ status_t Camera3Device::configureStreamsLocked(int operatingMode,
         sessionParams.unlock(metaBuffer);
         vCache->getVendorTagDescriptor(vendorId, &vTags);
         uint32_t tag;
-        if (CameraMetadata::getTagFromName(CAMERA_PACKAGE_NAME, vTags.get(), &tag)) {
-            ALOGE("%s: Unable to get %s tag", __FUNCTION__, CAMERA_PACKAGE_NAME);
+        if (CameraMetadata::getTagFromName(TAG_NAME, vTags.get(), &tag)) {
+            ALOGE("%s: Unable to get %s tag", __FUNCTION__, TAG_NAME);
         } else {
             std::string pkgName = CameraService::getCurrPackageName();
             status_t res = const_cast<CameraMetadata&>(sessionParams).update(tag, String8(pkgName.c_str()));
